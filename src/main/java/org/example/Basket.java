d6634d0a889cd0dff0e57ccb023572b53781408e
AlexGekko88 <115148133+AlexGekko88@users.noreply.github.com>
2023-03-31 12:52:28 +0300

    Add

---
+++ src/main/java/org/example/Basket.java
@@ -0,0 +1,43 @@
+package org.example;
+
+public class Basket {
+    protected Purchase[] purchases;
+
+    public Basket(int productsCount) {
+        this.purchases = new Purchase[productsCount];
+    }
+
+
+    public void addPurchase(Purchase purchase) {
+        for (int i = 0; i < purchases.length; i++) {
+            if (purchases[i] == null) {
+                purchases[i] = purchase;
+                return;
+            }
+            if (purchases[i].title.equals(purchase.title)) {
+                purchases[i].count += purchase.count;
+                return;
+            }
+        }
+    }
+
+    public int totalSum(Store store) { // параметр изменен на Store
+        int sum = 0;
+        Purchase purchase;
+        System.out.println("КОРЗИНА:");
+        for (int i = 0; i < purchases.length; i++) {
+            purchase = purchases[i];
+            if (purchase == null) break; //можно сразу прервать цикл и вернуть 0, не делая лишних итераций
+            int currentSum = purchase.purchaseSum(store);
+            System.out.println("\t" + purchase.title + " " + purchase.count + " шт. в сумме " + currentSum + " руб.");
+            sum += currentSum;
+        }
+        return sum;
+    }
+
+
+    public void finalPrint(Store store) { //добавлен отдельный метод вывода итоговой покупки на экран, чтобы не загромождать класс Main
+        System.out.println("Итого: " + this.totalSum(store));
+    }
+}
+
---
+++ src/main/java/org/example/Purchase.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Purchase {
+    protected String title;
+    protected int count;
+
+
+    public Purchase(String title, int count) {
+        this.title = title;
+        this.count = count;
+    }
+
+    public int purchaseSum(Store store) { //добавлен отдельный метод во избежание повтора кода (принцип DRY)
+        return this.count * store.getProducts().get(this.title);
+    }
+}
